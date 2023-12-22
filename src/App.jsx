06f24f3a8eeb86acbca3f5343b462502b5c3e479
guillermosifu <sifuentes1201@gmail.com>
2023-12-21 20:43:08 -0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-
-import Card from './Card'
-import Textos from './Textos'
-import Footer from './Footer'
-
+import Card from "./Card";
+import Textos from "./Textos";
+import Footer from "./Footer";
+import'./App.css'
 function App() {
- 
-
   return (
-    <div className='container'>
-   <Card/>
-   <Textos/>
-   <Footer/>
+    <div className="container">
+      <Card />
+      <Textos />
+      <Footer />
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
